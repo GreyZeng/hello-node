4d0b5bc5c5d845f3726e8c3eec24b35697fde47c
GreyZeng <410486047@qq.com>
2022-07-03 10:46:59 +0800

    simple express

---
+++ hello.js
@@ -0,0 +1,21 @@
+const x = 1
+const y = 2
+const z = 3
+console.count(
+    'x 的值为 ' + x + ' 且已经检查了几次？'
+)
+console.count(
+    'x 的值为 ' + x + ' 且已经检查了几次？'
+)
+console.count(
+    'y 的值为 ' + y + ' 且已经检查了几次？'
+)
+
+const oranges = ['橙子', '橙子']
+const apples = ['苹果']
+oranges.forEach(fruit => {
+    console.count(fruit)
+})
+/*apples.forEach(fruit => {
+    console.count(fruit)
+})*/
